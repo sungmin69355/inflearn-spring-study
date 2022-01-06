ea405e00fef8d2774f55a4b8fe6bd6cdec656016
sungmin69355 <sungmin69355@naver.com>
2022-01-06 20:34:15 +0900

    fix: AppConfig 리팩토링(역할을 분명하게 나눠줌)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberSerivce;
 import hello.core.member.MemberSerivceImpl;
@@ -10,11 +11,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     //설정을 여기서! DI(생성자 주입)
+    //역할을 분명하게! 보이도록 리팩토링 기존 코드에는 명확하게 보이지 않았다.
     public MemberSerivce memberSerivce(){
-        return new MemberSerivceImpl(new MemoryMemberRepository());
+        return new MemberSerivceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    //discountPolicy는 FixDiscountPolicy를 쓸것이 분명하게 나타남.
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
