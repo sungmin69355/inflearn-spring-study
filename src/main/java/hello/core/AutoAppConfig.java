5d06988ef98a9ac6190ae24d477cb8f739a60843
sungmin69355 <sungmin69355@naver.com>
2022-01-16 15:36:33 +0900

    fix: ComponentScan 사용 방법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan ( //Component를찾아서 알아서 컨테이너에 등록한다.
+        basePackages = "hello.core", //탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다.
+        basePackageClasses = AutoAppConfig.class, //지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
+        //만약 basePackages를 지정하지 않으면 @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
                 classes = Configuration.class)) // Configuration.class있는것을 뺴준다. AppConfig랑 충돌나면 안되기떄문에
 //보통 설정 정보를 컴포넌트 스캔 대상에서 제외하지는 않지만, 기존 예제 코드를 최대한 남기고 유지하기 위해서 이 방법을 선택했다.
