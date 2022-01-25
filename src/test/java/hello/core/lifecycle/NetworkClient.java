f4f58b8c7ef7821cb32a0735bcacfa8eac7d0eae
sungmin69355 <sungmin69355@naver.com>
2022-01-25 22:07:07 +0900

    fix: 빈초기화, 소멸 인터페이스 사용법

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,10 +1,12 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
-    public NetworkClient() { System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
     }
     public void setUrl(String url) {
         this.url = url;
@@ -19,5 +21,16 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void destroy() throws Exception { //빈이 종료될떄 실행
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception { //의존관계가 끝나면 실행
+        connect();
+        call("초기화 연결 메시지");
     }
 }
